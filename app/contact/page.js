f8324e1d2b8dacb53203b42c604c360b53374fc5
Adel Yasser <100410094+dola5xd@users.noreply.github.com>
2024-10-28 21:56:48 +0300

    Update page.js

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import ContactForm from "../_components/contactForm";
+import ContactForm from "../_components/ContactForm";
 import Link from "next/link";
 
 function page() {
